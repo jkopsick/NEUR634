6f0f1481d95d0db1ed66202f3cdd35c6516763a8
Jeffrey Kopsick <jeffk@localhost.localdomain>
2018-11-04 10:46:07 -0500

    Updated HW9 -- HW complete besides answering questions

--- class9/HW9_part45.py
+++ class9/HW9_part45.py
@@ -5,7 +5,7 @@ import moose
 import util as u
 from chan_proto_part1 import chan_set, rateParams
 
-# Define the parameters used to create the soma and dendrite compartments, and the pulse
+# Define the parameters used to create the soma and dendrite compartments
 EREST_ACT = -70e-3 #: Resting membrane potential  
 Em = EREST_ACT + 10.613e-3
 initVm = EREST_ACT
@@ -19,28 +19,26 @@ RM = 1/(0.3e-3*1e4)
 CM = 1e-6*1e4
 RA = 2
 
-# Create the soma and dendrite and connect them to each other
+# Create the soma and dendrite under the neutral neuron directory, and connect them to each other
 neuron = moose.Neutral('/neuron')
 soma = u.createCompartment(neuron,'swagginSoma',soma_l,soma_rad,RM,CM,RA,Em,Em)
 dend = u.createCompartment(neuron,'swagginDend',dend_l,dend_rad,RM,CM,RA,Em,Em)
 moose.connect(soma,'axial',dend,'raxial')
 
-
-# Define dictionaries for the excitatory channel, and the pre-synaptic neurons
+# Define dictionaries for the excitatory and inhibitory channels, and the pre-synaptic neurons
 glu = {'name': 'glu', 'Gbar' : 5e-9, 'tau1' : 1e-3, 'tau2' : 5e-3, 'erev' : 0}
 GABA = {'name': 'GABA', 'Gbar' : 1e-6, 'tau1' : 3e-3, 'tau2' : 5e-3, 'erev' : -80e-3}
 presyn1 = {'name': 'presyn1', 'rate' : 10, 'refractT' : 1e-3, 'delay' : 5e-3}
 presyn2 = {'name': 'presyn2', 'rate' : 70, 'refractT' : 1e-3, 'delay' : 5e-3}
 
-# Create the channels necessary for the giant squid axon and place them in a library
+# Create HH Na and K channels with their corresponding conductances, and place them in a library
+# so that they can be implemented into the compartments (if so desired)
 cond_set = {'Na': 120e-3*1e4, 'K': 36e-3*1e4}
 libraryName = '/library'
 compType = 'Compartment'
 u.createChanLib(libraryName, chan_set, rateParams, CaParams = None)
 
-
-# Create copies of the prototype channels and connect them to the
-# compartment, along with their conductances
+# Create copies of the prototype channels and connect them to each compartment under the neuron hierarchy
 u.addChannelSet(cond_set, libraryName, neuron, compType)
 
 # Create data tables to store the voltage for the soma and dendrite, and ignore current output
@@ -57,7 +55,7 @@ presyn1 = u.createRandSpike(presyn1, glu_handler)
 GABA_handler = u.addSynChan(dend, GABA)
 presyn2 = u.createRandSpike(presyn2, GABA_handler)
 
-# Create a table to store the output time of the spikes
+# Create tables to store the output time of the RandSpikes
 spike_table = moose.Table('st')
 moose.connect(presyn1, 'spikeOut', spike_table, 'spike')
 spike_table2 = moose.Table('st2')
@@ -77,5 +75,4 @@ t=np.linspace(0,simtime, len(dend_Vm.vector))
 plt.ion()
 plt.plot(t,soma_Vm.vector*1e3, 'r', label = 'soma Vm (mV)')
 plt.plot(t,dend_Vm.vector*1e3, 'b', label = 'dend Vm (mV)')
-
 plt.legend()
